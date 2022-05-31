9620542f6794b061388be1120dd450fd6ffb4337
trojan565 <majorexo565@gmail.com>
2022-05-31 17:37:18 +0300

    .

--- DiffingTest/contents.js
+++ DiffingTest/contents.js
@@ -1,14 +1,29 @@
 let PageTest={
     src:"PageTest.html",
-    type:"page",
-    name:"PageTest"
+    type:"static-page",
+    name:"Page-Test"
 }
 let DenemeSayfa={
     src:"DenemeSayfa.html",
     type:"page",
-    name:"DenemeSayfa",
+    name:"Deneme-sayfa",
+}
+let BaskaSayfa={
+    src:"baskaSayfa.html",
+    type:"page",
+    name:"Baska-Sayfa",
+}
+
+let atomDeneme={
+    type:"atom",
+    name:"atom-deneme",
+    data:()=>{
+        return x%5
+    }
 }
 
 let x = 0 ;
+let y = 0 ; 
 
-let contents=[PageTest,DenemeSayfa];
+let contents=[PageTest,DenemeSayfa,BaskaSayfa,atomDeneme];
+
