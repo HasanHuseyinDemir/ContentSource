a0a070b66944cfe650cb00aa72bece2d386c51be
trojan565 <majorexo565@gmail.com>
2022-06-01 15:28:04 +0300

    observe testleri gayet iyi çalışıyor

--- DiffingTest/contents.js
+++ DiffingTest/contents.js
@@ -18,11 +18,20 @@ window.y = 0 ;
 window.z = 100;
 
 window.args="Merhaba";
-/*
-dataSet.value.push("x","y","z","args","zx");*/
+
+dataSet.value.push("x","y","z","args","zx");
 let zx =0;
-let watched = new watch(["zx"],()=>zx>6?console.log(zx+" 6 dan büyük"):console.log(zx+"  6 dan küçük"));
-watchlist.push(watched);
+let xz =()=> zx%5;
+
+let watched = new watch(["zx","xz"],()=>zx>6?console.log(zx+" 6 dan büyük"):console.log(zx+"  6 dan küçük"));
+
+let XZ =new watch(["xz()"],()=>alert("Değişti!"));
+
+
+watchlist.push(watched,XZ);
+
+//setInterval(()=>{v()},300);
+
 
 let contents=[PageTest,atomDeneme];
 
