c2f526dd4613c49c53baa59faf003aca6b8f8ba3
trojan565 <majorexo565@gmail.com>
2022-06-01 14:12:43 +0300

    .

--- DiffingTest/contents.js
+++ DiffingTest/contents.js
@@ -19,7 +19,7 @@ window.z = 100;
 
 window.args="Merhaba";
 
-dataSet.value.push("x","y","z","args");
+dataSet.value.push("x","y","z","args","zx");
 let zx =0;
 let watched = new watch(["zx"],()=>zx>6?console.log(zx+" 6 dan büyük"):console.log(zx+"  6 dan küçük"));
 watchlist.push(watched);
