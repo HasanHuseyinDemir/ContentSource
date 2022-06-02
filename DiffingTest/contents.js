714bfbdc05b1a15cca0886b17dc36988273965e7
trojan565 <majorexo565@gmail.com>
2022-06-02 20:20:32 +0300

    attributeler eklendi!

--- DiffingTest/contents.js
+++ DiffingTest/contents.js
@@ -19,16 +19,11 @@ window.z = 100;
 
 window.args="Merhaba";
 
-dataSet.value.push("x","y","z","args","zx");
-let zx =0;
-let xz =()=> zx%5;
+dataSet.value.push("x","y","z","args");
 
-let watched = new watch(["zx","xz"],()=>zx>6?console.log(zx+" 6 dan büyük"):console.log(zx+"  6 dan küçük"));
+let watched = new watch(["y","x"],()=>alert());
 
-let XZ =new watch(["xz()"],()=>alert("Değişti!"));
-
-
-watchlist.push(watched,XZ);
+watchlist.push(watched);
 
 //setInterval(()=>{v()},300);
 
---
+++ vDomTest/app.js
@@ -0,0 +1,13 @@
+let PageTest={
+    src:"./page-test.html",
+    type:"page",
+    name:"page-test"
+}
+
+let x = 0;
+let y = 100;
+
+let samplelist=["selam","ben","hasan"];
+let merhaba = [{img:"imgs",src:"srcs"},{img:"Selam",src:"Selams"}]
+let contents = [PageTest];
+
